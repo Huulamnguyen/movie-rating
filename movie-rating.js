c7ddccd4fee26dcf1586e77371c353c37773e761
Huu Lam Nguyen <huulamnguyen@mail.adelphi.edu>
2020-07-29 17:09:07 -0400

    style with bootstrap

--- movie-rating.js
+++ movie-rating.js
@@ -2,14 +2,16 @@ $('#btn').on('click', function(){
     let input = $('#input').val();
     let score = $('#score').val();
     let removeBtn = $('<button type="submit">X</button>');    
-    $('<div id="rated">').appendTo($('#movie')).append(`${input} ${score}`)    
-    $('<button type="submit">X</button>').appendTo($('#movie'));
+    $('<div id="rated">').appendTo($('#movie')).append(`${input} `).append(`<span class=" badge badge-warning"> ${score}</span>`).attr('class','btn btn-primary btn-lg')    
+    $('<button class="btn btn-danger" type="submit">X</button>').appendTo($('#movie'));
+    $('#input').val('');
+    $('#score').val('');
 })
 
 //! CA YOU HELP ME REVISE THIS CODE?
-$('#movie').on('click',"button", function(){
-    $('button').prev('div').remove();
-    $('#movie button').remove();
+$('#movie').on('click',"button", function(e){
+    $(e.target).prev('div').remove();
+    $(e.target).remove();
 })
 
 
